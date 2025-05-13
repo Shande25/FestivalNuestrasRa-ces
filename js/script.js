4a2058e5ebe33952ea23b067db243c09f170c9dd
Shande <jahirparedes670@gmail.com>
2025-05-12 20:39:17 -0500

    Festival cambios 3

--- js/script.js
+++ js/script.js
@@ -74,6 +74,45 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+function showPreview(element) {
+    const previewBox = document.getElementById('previewBox');
+    const previewImg = document.getElementById('previewImg');
+    previewImg.src = element.src;
+    previewBox.style.display = 'block';
+  }
+
+  function closePreview() {
+    const previewBox = document.getElementById('previewBox');
+    previewBox.style.display = 'none';
+  }
+  const images = [
+    'image/diapositiva1/1.jpg',
+    'image/diapositiva1/2.jpg',
+    'image/diapositiva1/3.jpg',
+    'image/diapositiva1/4.jpg',
+    'image/diapositiva1/5.jpg',
+    'image/diapositiva1/6.jpg',
+    'image/diapositiva1/7.jpg'
+  ];
+  
+  let current = 0;
+
+  function openLightbox(index) {
+    current = index;
+    document.getElementById('lightbox-img').src = images[current];
+    document.getElementById('lightbox').style.display = 'block';
+  }
+
+  function closeLightbox() {
+    document.getElementById('lightbox').style.display = 'none';
+  }
+
+  function changeImage(step) {
+    current += step;
+    if (current < 0) current = images.length - 1;
+    if (current >= images.length) current = 0;
+    document.getElementById('lightbox-img').src = images[current];
+  }
 
 
 
